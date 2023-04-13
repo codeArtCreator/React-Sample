1d59d069757600c9b6218042958ffdbfed3f1cb9
Rakesh P R <rakeshpr369@gmail.com>
2023-04-13 17:00:23 +0530

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience. We&rsquo;re performing some maintenance at the moment. If you need to you can always follow us on <a href="http://www.twitter.com/">Twitter</a> for updates, otherwise we&rsquo;ll be back up shortly!</p>
+        <p>&mdash; Team codeArt</p>
+    </div>
+    </article>
+  );
+}
+
+export default Main;
